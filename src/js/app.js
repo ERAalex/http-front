4e5d5d975558052c275c6332118315455238fb78
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2024-03-09 14:45:42 +0300

    createTicket part

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,8 @@
 import TicketsWidget from "./ticket-widjet/ticket-widjet";
 
-
-
 document.addEventListener("DOMContentLoaded", () => {
 
-  // prepare UI interface to call widget
-  const ticketWidjet = new TicketsWidget(document.querySelector(".container"));
-
-  ticketWidjet.start();
-  ticketWidjet.setHandlers();
-
+  const container = document.querySelector(".container");
+  const ticketsWidget = new TicketsWidget(container)
+  ticketsWidget.start();
 });
--- src/js/ticket-widjet/ticket-widjet.js
+++ src/js/ticket-widjet/ticket-widjet.js
@@ -1,5 +1,5 @@
-
-import ApiServerCalls from "./api-widjet";
+import ApiServer from "../api-connect/api-widjet";
+import TicketCreation from "../widjet-structure/widjet-creation";
 
 export default class TicketsWidget {
   #currentTicket;
@@ -7,144 +7,41 @@ export default class TicketsWidget {
 
   constructor(container) {
     this.container = container;
-    this.ServerConnect = new ApiServerCalls("http://localhost:7070/");
+    this.ticketBase = new ApiServer("http://localhost:7070/");
 
-    // events on Create, Change, Delete
     this.addNewTicketBtn = container.querySelector(".add-ticket");
     this.ticketsContainer = container.querySelector(".tickets-container");
 
-
-
-
     this.addEditTicketPopup = container.querySelector(".add-edit-ticket");
-    this.addEditTicketTitle =
-      this.addEditTicketPopup.querySelector(".form-title");
-    this.addEditTicketName =
-      this.addEditTicketPopup.querySelectorAll(".form-input")[0];
-    this.addEditTicketDescription =
-      this.addEditTicketPopup.querySelectorAll(".form-input")[1];
+    this.addEditTicketTitle = this.addEditTicketPopup.querySelector(".form-title");
+    this.addEditTicketName = this.addEditTicketPopup.querySelectorAll(".form-input")[0];
+    this.addEditTicketDescription = this.addEditTicketPopup.querySelectorAll(".form-input")[1];
     this.addEditCloseBtn = this.addEditTicketPopup.querySelector(".form-reset");
     this.addEditAplyBtn = this.addEditTicketPopup.querySelector(".form-add");
 
     this.deleteTicketPopup = container.querySelector(".delete-ticket");
-    this.deleteTicketPopupCloseBtn =
-      this.deleteTicketPopup.querySelector(".form-reset");
-    this.deleteTicketPopupApplyBtn =
-      this.deleteTicketPopup.querySelector(".form-add");
+    this.deleteTicketPopupCloseBtn = this.deleteTicketPopup.querySelector(".form-reset");
+    this.deleteTicketPopupApplyBtn = this.deleteTicketPopup.querySelector(".form-add");
   }
-
-  start() {
-    // on load page check if any tickets
-    let allTickets = this.ServerConnect.init()
-
-    console.log(allTickets)
-
-    if (allTickets) {
-      for (const ticket of allTickets) {
-        console.log(ticket)
-        this.addNewTicket(ticket);
-      }
-    }
-  }
-
-  addNewTicket(ticket) {
-    const id = ticket.id;
-    const name = (ticket.name, 'ticket without name');
-    const description = (ticket.description, 'ticket without description');
-    const status = ticket.status;
-    const created = ticket.created;
-
-    this.ticketsContainer.appendChild(
-      this.createCard(id, name, description, status, created)
-    );
-  }
-
-  createCard(id, name, description, status, created) {
-    console.log('----name--1--')
-    console.log(name)
-
-    // create 2 parts of ticket div. 
-    //1 part information (id, name, description...)
-    //2 part update/delete buttons
-    const ticket = document.createElement("div");
-    const ticketInfo = document.createElement("div");
-    const ticketUpdateDelete = document.createElement("div");
-
-    ticket.appendChild(ticketInfo);
-    ticket.appendChild(ticketUpdateDelete);
-
-    ticket.classList.add("ticket");
-    ticketInfo.classList.add("ticket-info-part");
-    ticketUpdateDelete.classList.add("ticket-update-delete-part");
-    ticket.dataset.id = id;
-
-    // 1 part information
-    const ticketCheckbox = document.createElement("input");
-    ticketCheckbox.classList.add("ticket-checkbox");
-    ticketCheckbox.type = "checkbox";
-    ticketCheckbox.disabled = true;
-    ticketCheckbox.checked = true;
-    ticketInfo.appendChild(ticketCheckbox);
-
-    const ticketName = document.createElement("span");
-    ticketName.classList.add("ticket-name");
-    ticketName.textContent = name;
-    ticketName.dataset.id = id;
-    ticketInfo.appendChild(ticketName);
-
-    const ticketDescription = document.createElement("p");
-    ticketDescription.classList.add("ticket-description");
-    ticketDescription.textContent = description;
-    ticketInfo.appendChild(ticketDescription);
-
-    // 2 part update/delete buttons
-    const ticketTime = document.createElement("span");
-    ticketTime.classList.add("ticket-time");
-    ticketTime.textContent = created;
-    ticketUpdateDelete.appendChild(ticketTime);
-
-    const ticketEdit = document.createElement("button");
-    ticketEdit.classList.add("ticket-edit");
-    ticketEdit.dataset.id = id;
-    ticketUpdateDelete.appendChild(ticketEdit);
-
-    const ticketDelete = document.createElement("button");
-    ticketDelete.classList.add("ticket-delete");
-    ticketDelete.dataset.id = id;
-    ticketUpdateDelete.appendChild(ticketDelete);
-
-
-
-    return ticket;
-
-  }
-
 
   setHandlers() {
-    this.addNewTicketBtn.addEventListener(
-      "click",
-      this.onAddNewTicketBtn.bind(this)
-    );
-    this.addEditCloseBtn.addEventListener(
-      "click",
-      this.onAddEditCloseBtn.bind(this)
-    );
-    this.addEditAplyBtn.addEventListener(
-      "click",
-      this.onAddEditAplyBtn.bind(this)
-    );
-    this.ticketsContainer.addEventListener(
-      "click",
-      this.onTicketsContainer.bind(this)
-    );
-    this.deleteTicketPopupCloseBtn.addEventListener(
-      "click",
-      this.closeDeleteTicketWindow.bind(this)
-    );
-    this.deleteTicketPopupApplyBtn.addEventListener(
-      "click",
-      this.deleteTicket.bind(this)
-    );
+    this.addNewTicketBtn.addEventListener("click",this.onAddNewTicketBtn.bind(this));
+    this.addEditCloseBtn.addEventListener("click",this.onAddEditCloseBtn.bind(this));
+    this.addEditAplyBtn.addEventListener("click",this.onAddEditAplyBtn.bind(this));
+    this.ticketsContainer.addEventListener("click",this.onTicketsContainer.bind(this));
+    this.deleteTicketPopupCloseBtn.addEventListener("click",this.closeDeleteTicketWindow.bind(this));
+    this.deleteTicketPopupApplyBtn.addEventListener("click",this.deleteTicket.bind(this));
+  }
+
+  get addOrEdit() {return this.#addOrEdit;}
+  set addOrEdit(value) {this.#addOrEdit = value;}
+  get currentTicket() {return this.#currentTicket;}
+  set currentTicket(ticket) {this.#currentTicket = ticket;}
+
+  start() {
+    this.setHandlers();
+    this.ticketBase.getAllTickets(this.initTable.bind(this));
+    
   }
 
   initTable(allTickets) {
@@ -175,7 +72,7 @@ export default class TicketsWidget {
         this.ticketBase.createTicket(formName, formDescription, (data) => {
           const { id, name, description, status, created } = data;
           this.ticketsContainer.appendChild(
-            Ticket.createCard(id, name, description, status, created)
+            TicketCreation.createCard(id, name, description, status, created)
           );
         });
       } else if (this.addOrEdit === "edit") {
@@ -268,7 +165,10 @@ export default class TicketsWidget {
     });
   }
 
-
+  addNewTicket(ticket) {
+    const { id, name, description, status, created } = ticket;
+    this.ticketsContainer.appendChild(
+      TicketCreation.createCard(id, name, description, status, created)
+    );
+  }
 }
-
-
